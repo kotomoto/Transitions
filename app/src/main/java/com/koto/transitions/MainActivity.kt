3f6f915ad978eb7d7f5d12a6a2fa95d4614a8e99
Jacek Kotorowicz <jacek.kotorowicz@mobica.com>
2018-03-13 16:06:56 +0100

    Initial commit

---
+++ app/src/main/java/com/koto/transitions/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.koto.transitions
+
+import android.app.ActivityOptions
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity(), GridFragment.OnFragmentInteractionListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+//        val gridFragment = GridFragment()
+//        supportFragmentManager.beginTransaction().add(R.id.frame_container, gridFragment).commit()
+
+        new_activity_btn.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return when (item.itemId) {
+            R.id.action_settings ->
+                return true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
